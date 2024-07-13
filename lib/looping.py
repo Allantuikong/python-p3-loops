8315095143d942eaab558f7cd98375d888ee1373
Allan Kipkorir <allantuix@gmail.com>
2024-07-13 14:42:56 +0300

    Completed looping quiz

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,36 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    countdown = 10
+    while countdown > 0:
+        print(countdown)
+        countdown -= 1
+    print("Happy New Year!")
+
+happy_new_year()
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    return [num ** 2 for num in int_list]
+
+test_list = [1, 2, 3, 4, 5]
+result = square_integers(test_list)
+print(result)
+
+
+
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range (1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+
+        elif i % 3 == 0:
+            print("Fizz")
+
+        elif i % 5 == 0:
+            print("Buzz")
+
+        else:
+            print(i)
+
+fizzbuzz()
